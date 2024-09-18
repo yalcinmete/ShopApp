34f4c645c6d047d41b34b281e4f93eba909b4ee9
yalcinmete <yalcinmete.g4@gmail.com>
2024-09-18 14:07:06 +0300

    Admin Products sayfası oluşturuldu.

--- ShopApp.WebUI/Controllers/AdminController.cs
+++ ShopApp.WebUI/Controllers/AdminController.cs
@@ -1,12 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopApp.Business.Abstract;
+using ShopApp.Entities;
+using ShopApp.WebUI.Models;
 
 namespace ShopApp.WebUI.Controllers
 {
     public class AdminController : Controller
     {
+        private IProductService _productService;
+
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         public IActionResult Index()
         {
+            return View(new ProductListModel()
+            {
+                Products = _productService.GetAll()
+            });
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductModel model)
+        {
+            var entity = new Product()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Description = model.Description,
+                ImageUrl = model.ImageUrl,
+            };
+            _productService.Create(entity);
+            return Redirect("Index");
         }
     }
 }
