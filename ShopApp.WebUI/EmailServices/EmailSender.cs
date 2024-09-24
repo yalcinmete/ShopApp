1f2fa2fe2a9f7fb490c6d91205a00abecd5353bd
yalcinmete <yalcinmete.g4@gmail.com>
2024-09-24 17:47:23 +0300

    Email islemleri 2 devam

--- ShopApp.WebUI/EmailServices/EmailSender.cs
+++ ShopApp.WebUI/EmailServices/EmailSender.cs
@@ -1,13 +1,29 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace ShopApp.WebUI.EmailServices
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        private const string SendGridKey = "SG.jb5TUYAZSZ28H - z3b3TiuQ.PycF1_TslXKGIGjLheqx01Jz0lM0PJgNouih0O4fSr4";
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            throw new System.NotImplementedException();
+            
+            var apiKey = SendGridKey;
+            var client = new SendGridClient(SendGridKey);
+            var from = new EmailAddress("yalcinmete.g4@gmail.com", "Shop App");
+            var subject2 = subject;
+            var to = new EmailAddress("yalcinmete.g4@gmail.com", "Yalcin Mete");
+            var plainTextContent = "SendGrid ile test maili gönderildi.";
+            var htmlContent = htmlMessage;
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            var response = await client.SendEmailAsync(msg);
+
+            //return Task.FromResult(response);
         }
     }
 }
